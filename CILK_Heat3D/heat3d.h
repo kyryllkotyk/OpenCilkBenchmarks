8c50264daae4bae83d1b2fbd0dee7523c4c3e145
kyryllkotyk <105020476+kyryllkotyk@users.noreply.github.com>
2026-01-12 21:11:21 -0800

    Update and rename MASS_Heat3D to CILK_Heat3D/heat3d.h

---
+++ CILK_Heat3D/heat3d.h
@@ -0,0 +1,45 @@
+#ifndef HEAT_3D_
+#define HEAT_3D_
+
+#include <vector>
+#include <algorithm> // For swap
+
+#include <chrono> // For timing
+
+#include <cilk/cilk.h> // CILK
+
+using namespace std;
+class Heat3D
+{
+public:
+  Heat3D() = default;
+
+  /*
+    @brief Splits the given grid into blocks and calculates the heat value for every cell.
+    Modifies the given grid.
+
+    @param grid 1D array with the size of (gridX + 2) * (gridY + 2) * (gridZ + 2).
+    To convert from a 3D array, perform row flattening with z as the fast varying
+    index, INCLUDING the halo padding (0th and dimension size # elements)
+    @param gridX, gridY, gridZ Dimension size of the grid (Without padding)
+    @param timesteps How many steps (updates) to perform
+    @param alpha Constant value for the update formula for this cell's value importance
+    @param beta Constant value for the update formula for neighbor cell's value importance
+    @param decompX, decompY, decompZ Dimension size of the block to split grid into 
+    (Without padding)
+    @param totalRuns How many times to perform the same benchmark
+    @param warmupRuns How many times to perform the benchmark BEFORE recording
+
+    @return Halo time & execution time for each run
+  */
+  vector<pair<uint64_t, uint64_t>> runBenchmark(vector<float>& grid, 
+    const short gridX, const short gridY, const short gridZ, 
+    const short timesteps, const float alpha, const float beta,
+    const short decompX, const short decompY, const short decompZ,
+    const short totalRuns, const short warmupRuns);
+
+  // void runBenchmarkWithoutPadding
+  // void runBenchmarkFrom3DVector
+};
+
+#endif
