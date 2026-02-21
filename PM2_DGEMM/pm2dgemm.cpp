35175dd89076253c4e553d583fec6433bf4ac0d9
kyryllkotyk <105020476+kyryllkotyk@users.noreply.github.com>
2026-02-20 20:30:41 -0800

    Update pm2dgemm.cpp

--- PM2_DGEMM/pm2dgemm.cpp
+++ PM2_DGEMM/pm2dgemm.cpp
@@ -1,12 +1,34 @@
 #include "pm2dgemm.h"
 
-vector<vector<double>> PM2_DGEMM::runBenchmark(const short runs,
-    int aRowFullSize, int aColFullSize, int bRowFullSize, int bColFullSize,
-    int blockRowSize, int blockColSize,
-    int baseSeed) {
+inline pair<int,int> split1D(int totalDimensionSize, int processesInDimension, int index) {
+    int base = totalDimensionSize / processesInDimension;
+    // First extra blocks get +1 extra  
+    int extra = totalDimensionSize % processesInDimension; 
+    int count = base + (index < extra ? 1 : 0);
+    int start = index * base + (index < extra ? index : extra);
+    return { start, count };
+}
 
-    if (aColFullSize != bRowFullSize) {
-        cerr << "Cannot multiply, expected sizes: XxY, YxZ";
+inline double valueAt(int i, int j, int seed) {
+    uint64_t x = seed;
+    x ^= uint64_t(i) * 0x9e3779b97f4a7c15ULL;
+    x ^= uint64_t(j) * 0xbf58476d1ce4e5b9ULL;
+
+    x ^= (x >> 30);
+    x *= 0xbf58476d1ce4e5b9ULL;
+    x ^= (x >> 27);
+    x *= 0x94d049bb133111ebULL;
+    x ^= (x >> 31);
+
+    return (x >> 11) * (1.0 / (1ULL << 53));
+}
+
+vector<vector<double>> PM2_DGEMM::runBenchmark(const short runs,
+    int aSize, int bSize,
+    uint64_t baseSeed) {
+
+    if (aSize != bSize) {
+        cerr << "Cannot multiply, sizes must match";
         return {};
     }
 
@@ -18,26 +40,69 @@ vector<vector<double>> PM2_DGEMM::runBenchmark(const short runs,
     int size = 0;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Seed the randomizer for this process
-    // For uniqueness, add rank to the base seed
-    // Otherwise, same seed -> same values
-    Xoshiro256 rng(baseSeed + rank);
-
     // Turn the process list into a 2D array
     int dims[2] = { 0, 0 };
     MPI_Dims_create(size, 2, dims);
-    int processesRow = dims[0], processesCol = dims[1];
+   
+    if (dims[0] != dims[1]) {
+        cerr << "MPI created a non-square process grid."
+            << " Ensure that the ranks (-np #) is a perfect square.\n";
+    }
+
+    int processArrayOrder = dims[0];
 
     // Figure out grid dimensions
-    int myRow = rank / processesCol;
-    int myCol = rank % processesCol;
+    int myRow = rank / processArrayOrder;
+    int myCol = rank % processArrayOrder;
 
-    // Create the A,B,C matrices for this process
-    // blockSize x blockSize
-    vector<vector<double>> A, B, C;
+    // Find owned dimensions
+    pair<int, int> aLocalRows = split1D(aSize, processArrayOrder, myRow);
+    pair<int, int> aLocalCols = split1D(aSize, processArrayOrder, myCol);
 
-    // Fill grid of this process
-    
+    pair<int, int> bLocalRows = split1D(bSize, processArrayOrder, myRow);
+    pair<int, int> bLocalCols = split1D(bSize, processArrayOrder, myCol);
+
+    // Create grids
+    vector<vector<double>> aLocalGrid(aLocalRows.second, vector<double>(aLocalCols.second));
+    vector<vector<double>> bLocalGrid(bLocalRows.second, vector<double>(bLocalCols.second));
+    vector<vector<double>> cLocalGrid(aLocalRows.second, vector<double>(bLocalCols.second));
+
+    // Fill grid of this process with random values
+    for (int i = 0; i < aLocalRows.second; i++) {
+        for (int j = 0; j < aLocalCols.second; j++) {
+            aLocalGrid[i][j] = valueAt(i + aLocalRows.first, j + aLocalCols.first, baseSeed);
+        }
+    }
+    // Different base seed to prevent correlation
+    // The const is the 'golden ratio'
+    uint64_t bSeed = baseSeed ^ 0x9e3779b9;
+    for (int i = 0; i < bLocalRows.second; i++) {
+        for (int j = 0; j < bLocalCols.second; j++) {
+            bLocalGrid[i][j] = valueAt(i + bLocalRows.first,
+                j + bLocalCols.first, bSeed);
+        }
+    }
+
+    for (int k = 0; k < processArrayOrder; k++) {
+        if (k == myCol) 
+            //broadcast A for processes in the same row
+            // Buffer, count, MPI datatype, root, 
+            MPI_Bcast(aLocalGrid[0], aLocalRows, rank, MPI_DOUBLE, )
+        }
+        else {
+            //receive A from process[myRow][k]
+        }
+
+        if (k == myRow){
+            //broadcast B for processes in the same column
+        }
+        else {
+            //receive B from process[k][myCol]
+        }
+
+        //Perform calculation
+    }
+
     // P processes
     // Each Pij owns Aij, Bij, Cij
 
